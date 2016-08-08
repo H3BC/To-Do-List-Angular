a37ebf9d13d84a79aea5bad88385b656a0daad02
H3BC <konstanty.koszewski@gmail.com>
2016-08-08 13:14:35 +0200

    Add ability to edit and delete tasks

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -6,10 +6,10 @@ angular.module('app',[])
 	
 
 	$scope.addTask = function(){
-		if($scope.task != null || $scope.task != " "){
-			console.log(getCurrentDate())
+		if($scope.task != null){
 			$scope.taskList.push({'taskContent': $scope.task, 'status': 'false', 'time' : getCurrentDate() });
 			$scope.task = null;
+			
 		}
 		else{
 			return false;
@@ -19,6 +19,26 @@ angular.module('app',[])
 	$scope.editTask = function(){
 		event.target.contentEditable = event.target.contentEditable == 'true' ? 'false' : 'true';
 
+	}
+
+	$scope.closeEdit = function(space){
+		if (event.keyCode == 13 && space != " "){
+			$scope.editTask();
+			
+		}
+		
+	}
+
+	$scope.removeTask = function() {
+
+		var lngth = $scope.taskList.length;
+		for (var i = 0; i < lngth; i++){
+			console.log($scope.taskList[i].status);
+			if($scope.taskList[i].status == true){
+				$scope.taskList.splice(i, 1)
+				
+			}
+		}
 	}
 
 	function getCurrentDate(){
